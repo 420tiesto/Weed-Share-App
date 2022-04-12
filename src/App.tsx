9d43d62711404a4f8a37c4c2aea970437c640eb9
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-04-12 09:31:28 +0530

    setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import React from 'react'
 import { Provider } from "react-redux";
 import logo from "./logo.svg";
-import "./App.css";
 import store from "./app/state";
 import CreateProfile from './app/modules/socialMedia/profile/components/create-profile';
 import SignIn from './app/modules/socialMedia/auth/components/signIn/SignIn';

09b9341bae59f9e595bb746fb20819435addc8e9
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-04-17 09:17:27 +0530

    connect wallet step

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 	return (
 		<Provider store={store}>
 		<ConnectWallet/>
-		<Home/>
+		
 	</Provider>
 )
 }
---
+++ src/app/icons/CheckIcon.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+
+const CheckIcon = (props:any) => (
+  <svg
+    width={25}
+    height={22}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M23.621 1.346a2.421 2.421 0 0 1 .452 3.39l-12.01 15.747a2.417 2.417 0 0 1-3.632.241L.808 13.081a2.422 2.422 0 1 1 3.43-3.42l5.66 5.676L20.223 1.799a2.421 2.421 0 0 1 3.398-.453Z"
+      fill="#fff"
+    />
+  </svg>
+)
+
+export default CheckIcon

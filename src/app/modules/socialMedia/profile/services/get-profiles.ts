ee8f519c44a65a3b1445f42eebfcc630460fc926
Harrish-Selvarajah <selvarajahharrish@gmail.com>
2022-05-08 00:29:20 +0530

    added cinstant folder

--- src/app/modules/socialMedia/profile/services/get-profiles.ts
+++ src/app/modules/socialMedia/profile/services/get-profiles.ts
@@ -1,96 +1,17 @@
-import { apolloClient } from "../../../../services/apollo-client";
-import { gql } from "@apollo/client/core";
+import { apolloClient } from '../../../../services/apollo-client';
+import { gql } from '@apollo/client/core';
+import { GET_PROFILES } from '../../../../shared/constants';
 
-const GET_PROFILES = `
-  query($request: ProfileQueryRequest!) {
-    profiles(request: $request) {
-      items {
-        id
-        name
-        bio
-        location
-        website
-        twitterUrl
-        picture {
-          ... on NftImage {
-            contractAddress
-            tokenId
-            uri
-            verified
-          }
-          ... on MediaSet {
-            original {
-              url
-              mimeType
-            }
-          }
-          __typename
-        }
-        handle
-        coverPicture {
-          ... on NftImage {
-            contractAddress
-            tokenId
-            uri
-            verified
-          }
-          ... on MediaSet {
-            original {
-              url
-              mimeType
-            }
-          }
-          __typename
-        }
-        ownedBy
-        depatcher {
-          address
-          canUseRelay
-        }
-        stats {
-          totalFollowers
-          totalFollowing
-          totalPosts
-          totalComments
-          totalMirrors
-          totalPublications
-          totalCollects
-        }
-        followModule {
-          ... on FeeFollowModuleSettings {
-            type
-            amount {
-              asset {
-                symbol
-                name
-                decimals
-                address
-              }
-              value
-            }
-            recipient
-          }
-          __typename
-        }
-      }
-      pageInfo {
-        prev
-        next
-        totalCount
-      }
-    }
-  }
-`;
-
-export const getProfileHandleRequest = (handle: string) => {
-	return { handles: [handle], limit: 10 };
+export const getProfileByAddressRequest = (address: string) => {
+    getProfiles({ ownedBy: [address], limit: 10 });
+    // return { ownedBy: [address], limit: 10 };
 };
 
 export const getProfiles = (request: object) => {
-	return apolloClient.query({
-		query: gql(GET_PROFILES),
-		variables: {
-			request,
-		},
-	});
+    return apolloClient.query({
+        query: gql(GET_PROFILES),
+        variables: {
+            request,
+        },
+    });
 };

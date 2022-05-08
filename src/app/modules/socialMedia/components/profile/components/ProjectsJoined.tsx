40e6dff04f1ef05872d9607125a12478268c2fa0
Harrish-Selvarajah <selvarajahharrish@gmail.com>
2022-05-09 02:32:58 +0530

    Added routing for edit profile

--- src/app/modules/socialMedia/components/profile/components/ProjectsJoined.tsx
+++ src/app/modules/socialMedia/components/profile/components/ProjectsJoined.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import CardItem from './CardItem';
 
 interface Props {
-    ownedPublications: any[];
+    collectedPublications: any[];
 }
 
 const PLACEHOLDER_IMAGE_SRC =
     'https://images.unsplash.com/photo-1651694451626-cceed8ee5f33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80';
 
-const ProjectsCreated: React.FC<Props> = (props: Props) => {
-    const { ownedPublications } = props;
-    console.log(ownedPublications);
+const ProjectsJoined: React.FC<Props> = (props: Props) => {
+    const { collectedPublications } = props;
+    console.log(collectedPublications);
     return (
         <>
-            {ownedPublications && ownedPublications.length > 0 ? (
+            {collectedPublications && collectedPublications.length > 0 ? (
                 <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow  gap-8 p-4">
                     <CardItem
                         text="Cold Inner fire"
@@ -59,9 +59,9 @@ const ProjectsCreated: React.FC<Props> = (props: Props) => {
                     />
                 </div>
             ) : (
-                <div>No Projects Created</div>
+                <div>No Projects Joined</div>
             )}
         </>
     );
 };
-export default ProjectsCreated;
+export default ProjectsJoined;
--- src/app/modules/socialMedia/components/profile/pages/ProfilePage.tsx
+++ src/app/modules/socialMedia/components/profile/pages/ProfilePage.tsx
@@ -1,5 +1,7 @@
-import { Tab } from '@headlessui/react';
 import React, { useEffect } from 'react';
+import { useSetState } from 'react-use';
+import { Tab } from '@headlessui/react';
+
 import Avatar from '../components/Avatar';
 import ProjectsCreated from '../components/ProjectsCreated';
 import SearchBar from '../components/SearchBar';
@@ -8,8 +10,29 @@ import ProfileDetails from '../components/ProfileDetails';
 import ProfileSocials from '../components/ProfileSocials';
 import { getProfileByAddressRequest, getProfiles } from '../services/get-profiles';
 import { getPublications } from '../services/get-publications';
+import getIPFSUrlLink from '../../../../../utils/get-ipfs-url-link';
+import ProjectsJoined from '../components/ProjectsJoined';
+import { Link } from 'react-router-dom';
 
-const ProfilePage = () => {
+type Props = {};
+
+interface State {
+    profileDetails: any[];
+    ownedPublications: any[];
+    collectedPublications: any[];
+    loading: boolean;
+}
+
+const ProfilePage: React.FC<Props> = (props: Props) => {
+    const [state, setState] = useSetState<State>({
+        profileDetails: [],
+        ownedPublications: [],
+        collectedPublications: [],
+        loading: false,
+    });
+
+    const { profileDetails, loading, ownedPublications, collectedPublications } = state;
+
     useEffect(() => {
         getProfileDetails();
         getCollectedPublications();
@@ -25,7 +48,9 @@ const ProfilePage = () => {
             limit: 10,
         }).then((profile) => {
             console.log(profile.data);
+            setState({ profileDetails: profile.data.profiles.items });
             getProfilePublications(profile.data.profiles.items[0].id);
+            console.log(getIPFSUrlLink(profile.data.profiles.items[0].picture.original.url));
         });
     };
 
@@ -34,6 +59,7 @@ const ProfilePage = () => {
         getPublications({ profileId: id, publicationTypes: ['POST', 'COMMENT', 'MIRROR'] }).then(
             (publications) => {
                 console.log(publications, 'puvb');
+                setState({ ownedPublications: publications.data.publications.items });
             }
         );
     };
@@ -44,37 +70,86 @@ const ProfilePage = () => {
             publicationTypes: ['POST'],
         }).then((publications) => {
             console.log(publications, 'colectedpuvb');
+            setState({ collectedPublications: publications.data.publications.items });
         });
     };
 
     return (
         <div className="min-h-screen">
             <div className="overflow-hidden min-h-full  max-w-screen-xl mx-auto container sunken-element">
-                <div className="relative p-4">
-                    <img
-                        src="https://images.unsplash.com/photo-1490604001847-b712b0c2f967?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1853&q=80"
-                        alt=""
-                        className="w-full h-56 bg-dark-gray"
-                    />
+                <>
+                    {profileDetails && profileDetails.length > 0 ? (
+                        <div className="relative p-4">
+                            <img
+                                src="https://images.unsplash.com/photo-1490604001847-b712b0c2f967?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1853&q=80"
+                                alt=""
+                                className="w-full h-56 bg-dark-gray"
+                            />
 
-                    <div className="absolute top-36 pl-8 ">
-                        <Avatar imgSrc="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" />
-                    </div>
-                    <div className="flex justify-between  p-8">
-                        <ProfileDetails
-                            name="Memories of Moon"
-                            about="memories of moon"
-                            followerCount={50}
-                            followingCount={200}
-                        />
-                        <div className="flex flex-col gap-4 justify-end items-end">
-                            <a href="#" className="green-outline-btn px-4 max-w-fit">
-                                Edit Profile
-                            </a>
-                            <ProfileSocials fb="" google="" instagram="" shareLink="" twitter="" />
+                            <div className="absolute top-36 pl-8 ">
+                                <Avatar imgSrc={profileDetails[0].picture.original.url} />
+                            </div>
+                            <div className="flex justify-between  p-8">
+                                <ProfileDetails
+                                    name={profileDetails[0].name || profileDetails[0].handle}
+                                    about={`@${profileDetails[0].handle}`}
+                                    followerCount={profileDetails[0].stats.totalFollowing}
+                                    followingCount={profileDetails[0].stats.totalFollowers}
+                                />
+                                <div className="flex flex-col gap-4 justify-end items-end">
+                                    {/* <a href="#" className="green-outline-btn px-4 max-w-fit">
+                                        Edit Profile
+                                    </a> */}
+                                    <Link
+                                        className="green-outline-btn px-4 max-w-fit"
+                                        to="/profile-settings">
+                                        Edit Profile
+                                    </Link>
+                                    <ProfileSocials
+                                        fb=""
+                                        google=""
+                                        instagram=""
+                                        shareLink=""
+                                        twitter=""
+                                    />
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    ) : (
+                        <div className="relative p-4">
+                            <img
+                                src="https://images.unsplash.com/photo-1490604001847-b712b0c2f967?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1853&q=80"
+                                alt=""
+                                className="w-full h-56 bg-dark-gray"
+                            />
+
+                            <div className="absolute top-36 pl-8 ">
+                                <Avatar imgSrc="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" />
+                            </div>
+                            <div className="flex justify-between  p-8">
+                                <ProfileDetails
+                                    name="Memories of Moon"
+                                    about="memories of moon"
+                                    followerCount={50}
+                                    followingCount={200}
+                                />
+                                <div className="flex flex-col gap-4 justify-end items-end">
+                                    <a href="#" className="green-outline-btn px-4 max-w-fit">
+                                        Edit Profile
+                                    </a>
+                                    <ProfileSocials
+                                        fb=""
+                                        google=""
+                                        instagram=""
+                                        shareLink=""
+                                        twitter=""
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </>
+
                 <Tab.Group defaultIndex={0}>
                     <Tab.List className="elevated-element flex items-center justify-center gap-8">
                         <StyledTab>Projects Created</StyledTab>
@@ -87,9 +162,11 @@ const ProfilePage = () => {
 
                         <Tab.Panels>
                             <Tab.Panel>
-                                <ProjectsCreated />
+                                <ProjectsCreated ownedPublications={ownedPublications} />
                             </Tab.Panel>
-                            <Tab.Panel>Projects Joined</Tab.Panel>
+                            <Tab.Panel>
+                                <ProjectsJoined collectedPublications={collectedPublications} />
+                            </Tab.Panel>
                             <Tab.Panel>Timeline</Tab.Panel>
                             <Tab.Panel>Sound cloud songs</Tab.Panel>
                         </Tab.Panels>

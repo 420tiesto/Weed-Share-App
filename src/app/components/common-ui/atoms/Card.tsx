db2f0de8cc30e553a14951967f1d49b3d9c612c3
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-05-13 08:23:57 +0530

    Added Cards

--- src/app/components/common-ui/atoms/Card.tsx
+++ src/app/components/common-ui/atoms/Card.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import clsx from 'clsx';
+import React from 'react';
 
-type Props = {}
-
-const Card = (props: Props) => {
-  return (
-    <div>Card</div>
-  )
+interface CardProps {
+    children: React.ReactNode;
+    className?: string;
+    color?: 'light' | 'dark';
+    variant?: 'sunken' | 'elevated';
+    rounded?: 'none' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'full'
 }
 
-export default Card
+interface CardHeaderProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+interface CardBodyProps {
+    children?: React.ReactNode;
+    className?: string;
+}
+
+export const Card: React.FC<CardProps> = ({ children,rounded, className, color, variant }) => {
+    return (
+        <div
+            className={clsx(
+                {
+                    'elevated-element': variant === 'elevated',
+                    'sunken-element': variant === 'sunken',
+                    'bg-gray': color === 'light',
+                    'bg-dark-gray': color === 'dark',
+                },
+                `rounded-${rounded}`,
+                className
+            )}>
+            {children}
+        </div>
+    );
+};
+
+export const CardHeader: React.FC<CardHeaderProps> = ({ children, className = '' }) => {
+    return <div className={clsx(
+      'p-5 text-xl font-semibold border-b-2 border-white/25 ',
+      className,
+    )}>{children}</div>;
+};
+
+export const CardBody: React.FC<CardBodyProps> = ({ children, className = '' }) => {
+    return <div className={`p-5 ${className}`}>{children}</div>;
+};
--- src/app/components/common-ui/atoms/Stack.tsx
+++ src/app/components/common-ui/atoms/Stack.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import clsx from 'clsx';
+import React from 'react';
 
-type Props = {}
-
-const Stack = (props: Props) => {
-  return (
-    <div>Stack</div>
-  )
+interface StackProps {
+    children: React.ReactNode;
+    className?: string;
+    spacing?: 0 | 1 | 2 | 3 | 4 | 8;
 }
 
-export default Stack
+const Stack = ({ children, spacing = 2, className='' }: StackProps) => {
+    return (
+        <div
+            className={clsx(
+                {
+                    'gap-0': spacing === 0,
+                    'gap-1': spacing === 1,
+                    'gap-2': spacing === 2,
+                    'gap-3': spacing === 3,
+                    'gap-4': spacing === 4,
+                    'gap-8': spacing === 8,
+                },
+                'flex flex-col',
+                className
+            )}>
+            {children}
+        </div>
+    );
+};
+
+export default Stack;

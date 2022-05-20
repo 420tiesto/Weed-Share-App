b968cb0fff40de9bf2281f4cba8f8912e3c0bc4d
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-05-21 00:50:33 +0530

    feature : black loading for secondary button

--- src/app/components/common-ui/atoms/Spinner.tsx
+++ src/app/components/common-ui/atoms/Spinner.tsx
@@ -4,7 +4,7 @@ import clsx from 'clsx';
 interface Props {
     className?: string;
     size?: 'xs' | 'sm' | 'md' | 'lg'
-    variant?: 'primary' | 'secondary' | 'warning' | 'danger';
+    variant?: 'primary' | 'secondary' | 'warning' | 'danger' | 'black';
 }
 
 const Spinner: React.FC<Props> = ({ size = 'sm', variant = 'secondary', className = '' }) => {
@@ -16,6 +16,7 @@ const Spinner: React.FC<Props> = ({ size = 'sm', variant = 'secondary', classNam
                     'border-white/25 border-t-white': variant === 'secondary',
                     'border-yellow-500/25 border-t-yellow-400': variant === 'warning',
                     'border-red-500/25 border-t-red-500': variant === 'danger',
+                    'border-black/25 border-t-black': variant ==='black',
                     'h-4 w-4 border-[2px]': size === 'xs',
                     'h-5 w-5 border-2': size === 'sm',
                     'h-8 w-8 border-[3px]': size === 'md',

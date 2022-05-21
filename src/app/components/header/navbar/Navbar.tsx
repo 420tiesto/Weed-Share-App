af5bb35d9196daae2ec1dd79d6e6b1219812e378
Adarsh Patel <adarshzpatel@gmail.com>
2022-05-22 00:03:36 +0530

    Feature/pma 82 navbar revamp (#55)
    * navbar-revamp
    
    * fix

--- src/app/components/header/navbar/Navbar.tsx
+++ src/app/components/header/navbar/Navbar.tsx
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import UserMenu from '../userMenu/UserMenu';
-import Navlink from './Navlink';
+import Navlink, { AuthenticatedNavlink } from './Navlink';
 import { CREATE_PROJECT, EXPLORE, LOGIN } from '../../../routes/Routes';
 import { getUserAuthenticated, getUserHandle } from '../../../../modules/auth/state/auth.reducer';
 import { Link, useNavigate } from 'react-router-dom';
 import Button from '../../common-ui/atoms/Button';
-import { PlusIcon } from '@heroicons/react/outline';
+import { PlusIcon, SearchIcon } from '@heroicons/react/outline';
+import { Input } from '../../common-ui/atoms/Input';
 
 export type NavlinkType = {
     name: string;
@@ -22,15 +23,19 @@ const Navbar: React.FC = ({}) => {
     const authenticatedState = useSelector(getUserAuthenticated);
 
     const NAVBAR_LINKS: NavlinkType[] = [
-        { name: 'Home', href: '/' },
         { name: 'Explore', href: EXPLORE },
+        { name: 'Bounty', href: '/bounty' },
         // { name: 'Upload Album', href: CREATE_PROJECT },
         // {name:'Communities',href:'/communities'},
         // {name:'More',href:'/more'},
     ];
+    
+    const AUTHENTICATED_NAV_LINKS: NavlinkType[] = [
+        { name: 'Wallet', href: '/wallet' },
+        { name: 'Create', href: CREATE_PROJECT },
+    ];
 
     useEffect(() => {}, [authenticatedState]);
-
 
     const handleNavigate = () => {
         navigate(LOGIN);
@@ -40,22 +45,20 @@ const Navbar: React.FC = ({}) => {
             <nav className="flex items-center h-full justify-between">
                 {/* Logo */}
                 <img src="/prnts-logo.svg" alt="prnts" className="h-10" />
-                {/* User Menu */}
-                <div className="inline-flex pl-8 gap-2 flex-grow">
-                    {NAVBAR_LINKS.map(({ name, href }: NavlinkType) => (
-                        <Navlink href={href} name={name} key={name} />
-                    ))}
-                </div>
-                {authenticatedState ? (
-                    <div className="flex items-center gap-4">
-                       <Link to={'upload-album'}>
-                           <Button icon={<PlusIcon className='h-5 w-5' />} className='px-3 rounded-lg' >Create Project</Button>
-                       </Link> 
-                        <UserMenu />
+                <div className="flex gap-8 items-center flex-1 justify-end ">
+                    <div className="flex-grow max-w-md ">
+                        <Input leftIcon={<SearchIcon className="h-5 w-5" />} placeholder="Search" />
                     </div>
-                ) : (
-                    <Button onClick={handleNavigate}>Login</Button>
-                )}
+                    <div className="inline-flex pl-8 gap-2">
+                        {NAVBAR_LINKS.map(({ name, href }: NavlinkType) => (
+                            <Navlink href={href} name={name} key={name} />
+                        ))}
+                         {AUTHENTICATED_NAV_LINKS.map(({ name, href }: NavlinkType) => (
+                            <AuthenticatedNavlink authState={authenticatedState} href={href} name={name} key={name} />
+                        ))} 
+                    </div>
+                    {authenticatedState ? <UserMenu /> : <Button onClick={handleNavigate}>Login</Button> }
+                </div>
             </nav>
         </header>
     );
--- src/app/components/header/navbar/Navlink.tsx
+++ src/app/components/header/navbar/Navlink.tsx
@@ -3,17 +3,23 @@ import React, { useEffect } from 'react';
 import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import { NavlinkType } from './Navbar';
 
+type AuthNavlinkType = {
+    authState: boolean;
+    href: string;
+    name: string;
+};
+
 const Navlink = ({ href, name }: NavlinkType) => {
-  let resolved = useResolvedPath(href);
-  let match = useMatch({ path: resolved.pathname, end: true });
+    let resolved = useResolvedPath(href);
+    let match = useMatch({ path: resolved.pathname, end: true });
     return (
         <Link to={href}>
             <div
                 className={clsx(
                     {
                         'bg-primary/90 text-black ': match,
-                        'hover:bg-white/10 ':!match
-                      },
+                        'hover:bg-white/10 ': !match,
+                    },
                     'px-3 py-1 rounded-lg font-medium duration-200 ease-out transition-colors  '
                 )}>
                 {name}
@@ -23,3 +29,17 @@ const Navlink = ({ href, name }: NavlinkType) => {
 };
 
 export default Navlink;
+
+export const AuthenticatedNavlink = ({ authState=false, href, name }: AuthNavlinkType) => {
+    if (authState) {
+        return <Navlink href={href} name={name} />;
+    } else {
+        return (
+            <Link to={'/login'}>
+                <div className="px-3 hover:bg-white/10 py-1 rounded-lg font-medium duration-200 ease-out transition-colors ">
+                    {name}
+                </div>
+            </Link>
+        );
+    }
+};

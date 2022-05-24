40b1bcd2e56b6d71cd647abf8e32b9de0f6943c9
Harrish <selvarajahharrish@gmail.com>
2022-05-24 17:25:38 +0530

    PMA-76 added sentry error tracker (#61)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
 import App from './app/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import './types';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+    dsn: 'https://a6188aa4f0df48babae50aac64c1f23e@o1260323.ingest.sentry.io/6436337',
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
     <React.StrictMode>
